b1ec0dd42b65d2402b1b9c872901374f9ca97104
tom_global <tomabc@aliyun.com>
2021-03-27 12:23:59 +0800

    commit git 2

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * @author Administrator
+ * @date 2021/3/27 12:22
+ * @description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git !");
+        System.out.println("Hello git !");
+        System.out.println("Hello git !");
+    }
+}
